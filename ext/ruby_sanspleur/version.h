381bf7fefe4081defd41efc95b2b8d9eb9ff4db9
Jérôme Lebel <jerome@fotonauts.com>
2010-11-19 19:55:29 +0100

    moving to 1.0.6

--- ext/ruby_sanspleur/version.h
+++ ext/ruby_sanspleur/version.h
@@ -1,4 +1,4 @@
-#define RUBY_SANSPLEUR_VERSION  "1.0.5" // for easy parsing from rake files
+#define RUBY_SANSPLEUR_VERSION  "1.0.6" // for easy parsing from rake files
 #define RUBY_SANSPLEUR_VERSION_MAJ   1
 #define RUBY_SANSPLEUR_VERSION_MIN   0
-#define RUBY_SANSPLEUR_VERSION_MIC   5
+#define RUBY_SANSPLEUR_VERSION_MIC   6
