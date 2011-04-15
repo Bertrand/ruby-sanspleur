0007a2d0c5ae518aacb97d6aa19e5632ed431c85
Bertrand Guiheneuf <bertrand@fotonauts.com>
2011-04-15 17:25:11 +0200

    bump version

--- ext/ruby_sanspleur/version.h
+++ ext/ruby_sanspleur/version.h
@@ -1,4 +1,4 @@
-#define RUBY_SANSPLEUR_VERSION  "1.3.0" // for easy parsing from rake files
+#define RUBY_SANSPLEUR_VERSION  "1.4.0" // for easy parsing from rake files
 #define RUBY_SANSPLEUR_VERSION_MAJ   1
-#define RUBY_SANSPLEUR_VERSION_MIN   3
+#define RUBY_SANSPLEUR_VERSION_MIN   4
 #define RUBY_SANSPLEUR_VERSION_MIC   0
