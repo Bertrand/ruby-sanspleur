5d362d4c78ddb3a539aa6a49a7db5d43255b286c
Bertrand Guiheneuf <bertrand@fotonauts.com>
2013-05-30 19:20:47 +0200

    Update version.h
    Bump gem version

--- ext/ruby_sanspleur/version.h
+++ ext/ruby_sanspleur/version.h
@@ -1,4 +1,4 @@
-#define RUBY_SANSPLEUR_VERSION  "2.0.2" // for easy parsing from rake files
+#define RUBY_SANSPLEUR_VERSION  "2.1.0" // for easy parsing from rake files
 #define RUBY_SANSPLEUR_VERSION_MAJ   2
-#define RUBY_SANSPLEUR_VERSION_MIN   0
-#define RUBY_SANSPLEUR_VERSION_MIC   2
+#define RUBY_SANSPLEUR_VERSION_MIN   1
+#define RUBY_SANSPLEUR_VERSION_MIC   0
