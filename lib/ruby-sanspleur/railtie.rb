dd679e48a2a0d86a45f327bc1bb69b9067703a4b
Bertrand Guiheneuf <bertrand@fotonauts.com>
2013-05-31 14:46:30 +0200

    indentation

--- lib/ruby-sanspleur/railtie.rb
+++ lib/ruby-sanspleur/railtie.rb
@@ -1,8 +1,8 @@
 module RubySanspleur
   class Railtie < Rails::Railtie
 
-  	config.ruby_sanspleur = ActiveSupport::OrderedOptions.new
-	config.action_dispatch.rescue_responses['RubySanspleur::InvalidSecret'] = :unauthorized
+    config.ruby_sanspleur = ActiveSupport::OrderedOptions.new
+    config.action_dispatch.rescue_responses['RubySanspleur::InvalidSecret'] = :unauthorized
 
   end
 end
